08c953e27cfacf2e114b3c9141b9defea2b6ece1
phosah <efosa.uyi@gmail.com>
2025-06-26 15:35:04 -0700

    fix

--- test/unit/DipSaver.t.sol
+++ test/unit/DipSaver.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {DeployDipSaver} from "../../script/DeployDipSaver.s.sol";
